38f8694c9782a4269a28250eec7ea310f9492c4f
ArfinAlEusuf <arfinaleusuf@gmail.com>
2025-11-17 19:01:43 +0600

    adding snippets

---
+++ snippets.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    return 0;
+}
+
+// go to chorme and search for snippet generator
+//and make a snippet
+// go to settings and select c++
+// past the code and set a number after a prifix
+// set a $0 to start
+//than save 
