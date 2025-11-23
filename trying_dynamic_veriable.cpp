6a2621b58bea0b7a7f2abb25fbfaed506ffe57f8
ArfinAlEusuf <arfinaleusuf@gmail.com>
2025-11-23 19:04:12 +0600

    taking input and passing from fun to main fun

--- trying_dynamic_veriable.cpp
+++ trying_dynamic_veriable.cpp
@@ -1,14 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main()
+int *input(int n)
 {
-    int n;
-    cin >> n;
     int *a = new int[n];
     for (int i = 0; i < n; i++)
     {
         cin >> a[i];
     }
+    return a;
+}
+int main()
+{
+    int n;
+    cin >> n;
+    int *a = input(n);
     for (int i = 0; i < n; i++)
     {
         cout << a[i] << " ";
